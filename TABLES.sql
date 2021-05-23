95dc17b62357670d09dff52e4bb5ec300f8f25b3
PleaBanshee <llewellynant@gmail.com>
2021-05-23 18:50:14 +0200

    Major modification of CREATE.sql and INSERTIONS.sql

--- TABLES.sql
+++ TABLES.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Department (
     Dep_ID NUMBER(10) CONSTRAINT PK_DEPARTMENT PRIMARY KEY,
     Dep_Name VARCHAR(50) NOT NULL,
-    Dep_ContactNumber VARCHAR(12)  CHECK (LENGTH(Dep_ContactNumber) = 12 AND Dep_ContactNumber LIKE '+27%') UNIQUE NOT NULL
+    Dep_ContactNumber VARCHAR(12) UNIQUE NOT NULL,
+    CONSTRAINT CHECK_PHONE CHECK (LENGTH(Dep_ContactNumber) = 12 AND Dep_ContactNumber LIKE '+27%') 
 );
 
 CREATE TABLE Shift (
@@ -9,15 +10,17 @@ CREATE TABLE Shift (
     Shift_Description VARCHAR(50) NOT NULL,
     Shift_Start DATE NOT NULL,
     Shift_End DATE NOT NULL,
-    ADD CONSTRAINT VALID_SHIFTS CHECK (Shift_End > Shift_Start)
+    CONSTRAINT VALID_SHIFTS CHECK (Shift_End > Shift_Start)
 );
 
 CREATE TABLE Mine (
     Mine_ID NUMBER(10) CONSTRAINT PK_MINE PRIMARY KEY, 
     Mine_Name VARCHAR(50) UNIQUE NOT NULL,
     Location VARCHAR(120) NOT NULL, 
-    Contact_Num VARCHAR(12) CHECK (LENGTH(Contact_Num) = 12 AND Contact_Num LIKE '+27%') UNIQUE NOT NULL,
-    Coal_Quantity BINARY_FLOAT CHECK (Coal_Quantity >= 0) NOT NULL
+    Contact_Num VARCHAR(12)  UNIQUE NOT NULL,
+    Coal_Quantity BINARY_FLOAT NOT NULL,
+    CONSTRAINT CHECK_MINENUM CHECK (LENGTH(Contact_Num) = 12 AND Contact_Num LIKE '+27%'),
+    CONSTRAINT CHECK_QUANTITY CHECK (Coal_Quantity >= 0) 
 );
 
 CREATE TABLE Qualification_Detail (
@@ -37,13 +40,16 @@ CREATE TABLE Employee (
     Dep_ID NUMBER(10) NOT NULL, 
     Shift_ID NUMBER(10) NOT NULL,
     Mine_ID  NUMBER(10) NOT NULL,
-    Salary BINARY_FLOAT CHECK (Salary >= 15000) NOT NULL,      
+    Salary BINARY_FLOAT NOT NULL,      
     Emp_FirstName VARCHAR(50) NOT NULL,   
     Emp_LastName VARCHAR(50) NOT NULL,      
-    Emp_ContactNumber VARCHAR(12) CHECK (LENGTH(Emp_ContactNumber) = 12 AND Emp_ContactNumber LIKE '+27%') UNIQUE NOT NULL,
+    Emp_ContactNumber VARCHAR(12) UNIQUE NOT NULL,
     Emp_Address VARCHAR(100) NOT NULL,     
-    Emp_Email  VARCHAR(100) CHECK (Emp_Email LIKE '%@gmail%' OR Emp_Email LIKE '%@hotmail%'  AND Emp_Email LIKE '%.com') NOT NULL,  UNIQUE NOT NULL,     
+    Emp_Email  VARCHAR(100) UNIQUE NOT NULL,      
     Job_Description VARCHAR(50) NOT NULL,
+    CONSTRAINT CHECK_SAL CHECK (Salary >= 15000),
+    CONSTRAINT CHECK_PHONENUM CHECK (LENGTH(Emp_ContactNumber) = 12 AND Emp_ContactNumber LIKE '+27%'),
+    CONSTRAINT CHECK_EMAIL CHECK (Emp_Email LIKE '%@gmail%' OR Emp_Email LIKE '%@hotmail%'  AND Emp_Email LIKE '%.com'),
     CONSTRAINT FK_DEPARTMENT FOREIGN KEY (Dep_ID) REFERENCES Department(Dep_ID),
     CONSTRAINT FK_SHIFT FOREIGN KEY (Shift_ID) REFERENCES Shift(Shift_ID),
     CONSTRAINT FK_MINE FOREIGN KEY (Mine_ID) REFERENCES Mine(Mine_ID)
@@ -51,15 +57,18 @@ CREATE TABLE Employee (
 
 CREATE TABLE Export (
     Export_ID NUMBER(10) CONSTRAINT PK_EXPORT PRIMARY KEY, 
-    Quantity BINARY_FLOAT CHECK (Quantity >= 1000 AND Quantity <= 120000) NOT NULL,    
+    Quantity BINARY_FLOAT NOT NULL,    
     Transporter VARCHAR(50) NOT NULL,  
-    Transporter_Stat CHAR(1) CHECK (Transporter_Stat = 'A' OR Transporter_Stat = 'I') NOT NULL,
+    Transporter_Stat CHAR(1) NOT NULL,
     Ship_Date DATE NOT NULL,    
     Arrival_Date DATE NOT NULL, 
     Sales_Price BINARY_FLOAT NOT NULL,
     Export_Cost BINARY_FLOAT NOT NULL, 
     Dest_ID NUMBER(10) NOT NULL,
     Mine_ID NUMBER(10) NOT NULL,
+    CONSTRAINT CHECK_COAL CHECK (Quantity >= 1000 AND Quantity <= 120000),
+    CONSTRAINT CHECK_STATUS CHECK (Transporter_Stat = 'A' OR Transporter_Stat = 'I'),
+    CONSTRAINT CHECK_EXPORTS CHECK (Arrival_Date >= Ship_Date),
     CONSTRAINT FK_DESTINATION FOREIGN KEY (Dest_ID) REFERENCES Destination(Dest_ID),
     CONSTRAINT FK_MINING FOREIGN KEY (Mine_ID) REFERENCES Mine(Mine_ID),
     CONSTRAINT VALID_EXPORTS CHECK (Ship_Date <= Arrival_Date)
@@ -74,7 +83,8 @@ CREATE TABLE Manager (
 CREATE TABLE Qualification (
     Qual_ID NUMBER(10) REFERENCES Qualification_Detail(Qual_ID),
     Emp_ID NUMBER(10) REFERENCES Employee(Emp_ID),
-    Qualification_Num VARCHAR(10)  CHECK (Qualification_Num = 10) UNIQUE NOT NULL,
+    Qualification_Num VARCHAR(10) UNIQUE NOT NULL,
+    CONSTRAINT CHECK_QUALNUM CHECK (Qualification_Num = 10),
     CONSTRAINT PK_QUALIFICATION PRIMARY KEY (Qual_ID,Emp_ID)
 );
 
